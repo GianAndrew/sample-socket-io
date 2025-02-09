de9135d73377c7040cd130db52adc627f511208f
Gian Del Rosario <giandrewdelrosario@gmail.com>
2025-02-09 18:50:45 +0800

    netlify files was removed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const http = require('http');
 const serverless = require('serverless-http');
 const { Server } = require('socket.io');
 const morgan = require('morgan');
-const router = express.Router();
 const app = express();
 const server = http.createServer(app);
 
@@ -13,6 +12,7 @@ const io = new Server(server, {
         origin: 'http://localhost:5173',
         methods: ['GET', 'POST'],
     },
+    connectionStateRecovery: {},
 });
 
 app.use(express.json());
@@ -23,8 +23,6 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
-app.use('./netlify/functions/src', router);
 
 io.on('connection', (socket) => {
     console.log('User connected', socket.id);
@@ -38,9 +36,17 @@ io.on('connection', (socket) => {
         socket.to(data.room).emit('receiveMessage', data);
         console.log(data);
     });
-    socket.on('disconnect', () => {
+    socket.on('disconnect', (reason) => {
         console.log('User disconnected', socket.id);
+        console.log('Reason: ' + reason);
     });
+
+    if (socket.recovered) {
+        socket.on('sendMessage', (data) => {
+            socket.to(data.room).emit('receiveMessage', data);
+            console.log(data);
+        });
+    }
 });
 
 module.exports = app;
